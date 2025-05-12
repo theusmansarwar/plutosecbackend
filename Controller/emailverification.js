a2146e2502a7d9bf21b05fc662eb57a5e70e4b15
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-05-12 14:41:52 +0500

    smtp changed

--- Controller/emailverification.js
+++ Controller/emailverification.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.fastmail.com", // GoDaddy SMTP server
+  host: "smtp.zoho.com", // GoDaddy SMTP server
   port: 465,
   secure: true,
   auth: {
