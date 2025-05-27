98cf77812fe72b1c68c8fbbfef4f0170c4369d27
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-05-27 16:30:22 +0500

    users

--- Controller/authController.js
+++ Controller/authController.js
@@ -58,7 +58,7 @@ const register = async (req, res) => {
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password: password,
       type: {
         _id: userType._id,
         name: userType.name,
