438926ba74d30bbc0b4132bd556102661d732b84
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-05-27 14:46:08 +0500

    users

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -19,6 +19,6 @@ router.get("/users", authMiddleware, getAllUsers);
 router.get("/users/:id", authMiddleware, getUserById);
 router.put("/users/:id", authMiddleware, updateUser); 
 router.delete("/users/:id", authMiddleware, deleteUser);
-router.delete("/users/deleteMultiple", authMiddleware, deleteMultipleUsers);
+router.post("/users/deleteMultiple", authMiddleware, deleteMultipleUsers);
 
 module.exports = router;
