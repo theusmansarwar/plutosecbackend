fee5f506b87b4a44f381bc51b8dcf8656efa928c
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-06-03 13:12:20 +0500

    users and admin tickets

--- Controller/ticketController.js
+++ Controller/ticketController.js
@@ -1,6 +1,6 @@
 const Leads = require("../Models/leadsModel");
 const Tickets = require("../Models/ticketModel");
-const {sendEmailToUser} = require("./emailverification");
+const { sendEmailToUser } = require("./emailverification");
 const axios = require("axios");
 
 const CreateTicket = async (req, res) => {
@@ -22,15 +22,23 @@ const CreateTicket = async (req, res) => {
     const LeadsData = await Leads.findById(id);
     const totalTickets = await Tickets.countDocuments();
     const generateTicket = await Tickets.create({
-      ticketNO: totalTickets.toString().padStart(4, '0'),
+      ticketNO: totalTickets.toString().padStart(4, "0"),
       clientemail: LeadsData.email,
       clientname: LeadsData.name,
-      receivername:"PlutoSec Support Team",
-      receiveremail:"theusmansarwar26@gmail.com",
-      subject:LeadsData.subject,
+      receivername: "PlutoSec Support Team",
+      receiveremail: "theusmansarwar26@gmail.com",
+      subject: LeadsData.subject,
       status: false,
     });
-    sendEmailToUser({ TicketId:generateTicket._id, clientemail: LeadsData.email,name:LeadsData.name, ticketNO:generateTicket.ticketNO}, res);
+    sendEmailToUser(
+      {
+        TicketId: generateTicket._id,
+        clientemail: LeadsData.email,
+        name: LeadsData.name,
+        ticketNO: generateTicket.ticketNO,
+      },
+      res
+    );
 
     return res.status(201).json({
       status: 201,
@@ -48,12 +56,11 @@ const CreateTicket = async (req, res) => {
 
 const GetTicketById = async (req, res) => {
   try {
-    const { id } = req.params; 
+    const { id } = req.params;
 
     const ticket = await Tickets.findById(id).populate({
-        path: "chats",
-       
-      });
+      path: "chats",
+    });
 
     if (!ticket) {
       return res.status(404).json({
@@ -77,17 +84,21 @@ const GetTicketById = async (req, res) => {
 };
 const listtickets = async (req, res) => {
   try {
-   
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
     const ticketslist = await Tickets.find()
       .select("-chats")
-        .sort({ status: 1, updatedAt: -1 });
-      
+      .sort({ status: 1, updatedAt: -1 })
+      .limit(limit)
+      .skip((page - 1) * limit);
 
     const totalTickets = await Tickets.countDocuments();
 
     res.status(200).json({
       totalTickets,
-     
+      totalPages: Math.ceil(totalTickets / limit),
+      currentPage: page,
+      limit:limit,
       ticketslist,
     });
   } catch (error) {
@@ -104,5 +115,5 @@ module.exports = {
   CreateTicket,
 
   GetTicketById,
-  listtickets
+  listtickets,
 };
